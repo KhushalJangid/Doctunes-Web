7ee573dc2c15e4cb14553d9dc52016f9c328b5f7
KhushalJangid <kjangid897@gmail.com>
2023-08-01 15:09:44 +0530

    Final changes

---
+++ js/contact.js
@@ -0,0 +1,25 @@
+const db = getFirestore(app);
+import {
+    collection,
+    addDoc
+} from "firebase/firestore";
+
+//Get Form Values
+const name = document.getElementById('name').value
+const email = document.getElementById('email').value
+const message = document.getElementById('message').value
+
+const submitButton = document.getElementById('submit')
+
+submitButton.addEventListener("click", async (e) => {
+    try {
+    const docRef = await addDoc(collection(db, "contactQuery"), {
+        Name: name,
+        Email: email,
+        Message: message
+    });
+    console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+    console.error("Error adding document: ", e);
+    }
+})
--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 $(function () {
   $(document).scroll(function () {
     var $nav = $(".navbar");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > ($(window).height()*0.8));
+    $nav.toggleClass('scrolled', $(this).scrollTop() > ($(window).height() * 0.8));
   });
 });
 
 var acc = document.getElementsByClassName("accordion");
 
 for (var i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("accordian-active");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
@@ -19,22 +19,69 @@ for (var i = 0; i < acc.length; i++) {
   });
 }
 
-document.querySelector('#navbar-toggle').addEventListener("click",()=>{
+document.querySelector('#navbar-toggle').addEventListener("click", () => {
   var drawer = document.querySelector('#nav-content');
   var icon = document.querySelector('#icon');
+  drawer.childNodes.forEach((node) => {
+
+    node.addEventListener("click", () => {
+      icon.classList.add('down');
+      icon.classList.remove('up');
+      drawer.classList.remove("navbar-expanded");
+      drawer.classList.add("navbar-collapsed");
+      drawer.attributes.collapsed.value = "true";
+    });
+  });
   if (drawer.attributes.collapsed.value == "true") {
     icon.classList.remove('down');
     icon.classList.add('up');
     drawer.classList.add("navbar-expanded");
     drawer.classList.remove("navbar-collapsed");
     drawer.attributes.collapsed.value = "false";
-}else{
-  icon.classList.add('down');
+  } else {
+    icon.classList.add('down');
     icon.classList.remove('up');
-  drawer.classList.remove("navbar-expanded");
-
+    drawer.classList.remove("navbar-expanded");
     drawer.classList.add("navbar-collapsed");
     drawer.attributes.collapsed.value = "true";
-}
+  }
 
 });
+
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on('click', function (event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+function download() {
+  var userAgent = navigator.userAgent || window.opera;
+  // iOS detection from: http://stackoverflow.com/a/9039885/177710
+  if (/iPad|iPhone|iPod|Macintosh|Apple/.test(userAgent) && !window.MSStream) {
+    console.log("ios");
+    window.open('https://apps.apple.com/us/app/doctunes-pdf-text-to-speech/id6443713628');
+  } else {
+    console.log("android");
+    window.open('https://play.google.com/store/apps/details?id=com.boss.audiobookconverter');
+
+  }
+}
